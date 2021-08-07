877f24c3ca4b5bcdbd033137da07ab8d5300c0bb
GodusOV <78070496+GodusOV@users.noreply.github.com>
2021-08-07 14:23:32 +0800

    Update settings.py

--- OVBot/settings.py
+++ OVBot/settings.py
@@ -125,12 +125,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_root/')
 
-STATICFILES_DIRS = [
-    STATIC_ROOT,
-]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
