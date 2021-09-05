5e81d8fedadeda2c425b4f0263527058eefd23ff
GodusOV <78070496+GodusOV@users.noreply.github.com>
2021-09-05 20:24:42 +0800

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -1,13 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from flask import Flask
 from flask import request
 from pymongo import MongoClient
+import requests
 
 client = MongoClient('mongodb+srv://godusov:ZSuD1SP4RBHNlZa3@maincluster.zurxi.mongodb.net/maindb?retryWrites=true&w=majority')
 db = client['maindb']
 cll = db['userdata']
 dcll = db['userdonatedata']
 gcll = db['guilddata']
+discordauthurl = "https://discord.com/api/oauth2/authorize?client_id=863028787708559400&redirect_uri=https%3A%2F%2Fovbotdiscord.herokuapp.com%2Fconfirm-user&response_type=code&scope=identify"
 
 # Create your views here.
 def homepage(request):
@@ -26,16 +28,6 @@ def propack(request):
 
 def hpack(request):
 	return render(request, "Hacker-Pack.html", {})
-
-def confirm(request):
-	if request.method == 'POST':
-		userid = int(request.POST.get('userid'))
-		if cll.find_one({"id": userid}) == None:
-			return render(request, "Cannot-find-user.html", {'userid': userid})
-		else:
-			user = cll.find_one({"id": userid})
-			username = user['name']
-			return render(request, "Confirm-user.html", {'username': username, 'userid': userid})
 
 def cfuser(request):
 	return render(request, "Cannot-find-user.html", {})
@@ -74,3 +66,37 @@ def pfail(request):
 
 def verify(request):
 	return render(request, "A6085E730C40707DBAFA47AB2118475E.txt", {})
+
+def login(request):
+	return redirect(discordauthurl)
+
+def confirm(request):
+	code = request.GET.get('code')
+	user = exchangecode(code)
+	userid = user['id']
+	if request.method == 'POST':
+		if cll.find_one({"id": userid}) == None:
+			return render(request, "Cannot-find-user.html", {'userid': userid})
+		else:
+			user = cll.find_one({"id": userid})
+			username = user['name']
+			return render(request, "Confirm-user.html", {'username': username, 'userid': userid})
+
+def exchangecode(code: str):
+	data = {
+		'client_id': '863028787708559400',
+		'client_secret': '2nE2Ck8aupqxQ2Wi7LVS2GF4ff-nq3jA',
+		'grant_type': 'authorization_code',
+		'code': code,
+		'redirect_uri': 'https://ovbotdiscord.herokuapp.com/confirm-user/',
+		'scope': 'identify'
+	}
+	headers = {
+		'Content-Type': 'application/x-www-form-urlencoded'
+	}
+	response = requests.post("https://discord.com/api/oauth2/token", data=data, headers=headers)
+	credentials = response.json()
+	access_token = credentials['access_token']
+	response = requests.get("https://discord.com/api/v6/users/@me", headers = {"Authorization": "Bearer %s" % access_token})
+	user = response.json()
+	return user
