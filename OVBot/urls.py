2d4cd784aa5510f002cbb4b02a725b23edb3ddd8
GodusOV <78070496+GodusOV@users.noreply.github.com>
2021-09-05 20:25:39 +0800

    Update urls.py

--- OVBot/urls.py
+++ OVBot/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from pages.views import homepage, dcase, dpack, propack, hpack, confirm, cfuser, psuccess, pfail, verify
+from pages.views import homepage, dcase, dpack, propack, hpack, confirm, cfuser, psuccess, pfail, verify, login
 
 from django.conf import settings
 from django.views.static import serve
@@ -19,6 +19,7 @@ urlpatterns = [
     path('purchase-success/', psuccess, name="purchase-success"),
     path('purchase-fail/', pfail, name="purchase-fail"),
     path('.well-known/pki-validation/A6085E730C40707DBAFA47AB2118475E.txt', verify, name='verify'),
+    path('login/', login, name='login'),
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
