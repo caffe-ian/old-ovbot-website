b8e295c0c23019aa5701945139ac4eaaacb77a99
GodusOV <78070496+GodusOV@users.noreply.github.com>
2021-09-05 20:35:53 +0800

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -74,13 +74,12 @@ def confirm(request):
 	code = request.GET.get('code')
 	user = exchangecode(code)
 	userid = user['id']
-	if request.method == 'POST':
-		if cll.find_one({"id": userid}) == None:
-			return render(request, "Cannot-find-user.html", {'userid': userid})
-		else:
-			user = cll.find_one({"id": userid})
-			username = user['name']
-			return render(request, "Confirm-user.html", {'username': username, 'userid': userid})
+	if cll.find_one({"id": userid}) == None:
+		return render(request, "Cannot-find-user.html", {'userid': userid})
+	else:
+		user = cll.find_one({"id": userid})
+		username = user['name']
+		return render(request, "Confirm-user.html", {'username': username, 'userid': userid})
 
 def exchangecode(code: str):
 	data = {
