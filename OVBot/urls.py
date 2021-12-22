e267ad47091cac66ff0ca9a23d7b64fa7713e260
Chock1 <78070496+Chock1@users.noreply.github.com>
2021-12-22 15:26:30 +0800

    Update urls.py

--- OVBot/urls.py
+++ OVBot/urls.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 from django.urls import path, include
-from pages.views import homepage, confirm, cfuser, psuccess, pfail, verify, login, gift, privacy, paypaljs
+from pages.views import homepage, confirm, cfuser, psuccess, pfail, verify, login, gift, privacy, paypaljsg, paypaljsc
 
 from django.conf import settings
 from django.views.static import serve
@@ -18,7 +18,8 @@ urlpatterns = [
     path('login/', login, name='login'),
     path('gifting-user/', gift, name="gifting-user"),
     path('privacy-policy/', privacy, name="privacy-policy"),
-    path('paypaljs.js', paypaljs, name='paypaljs')
+    path('paypaljsg.js', paypaljsg, name='paypaljs')
+    path('paypaljsc.js', paypaljsc, name='paypaljs')
 
     url(r'^media/(?P<path>.*)$', serve,{'document_root':       settings.MEDIA_ROOT}), 
     url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}), 
