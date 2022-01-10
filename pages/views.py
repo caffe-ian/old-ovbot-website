2a95cb9eac1952b8de391500febc98c03ffb4cd8
Chock1 <78070496+Chock1@users.noreply.github.com>
2022-01-10 19:25:23 +0800

    Update views.py

--- pages/views.py
+++ pages/views.py
@@ -110,7 +110,11 @@ def gift(request):
 		userid = int(request.POST.get('userid'))
 		gifterid = int(request.POST.get('gifterid'))
 		if cll.find_one({"id": userid}) == None:
-			return render(request, "Cannot-find-user.html", {'userid': userid})
+			if userid == gifterid:
+				reason = "Why are you gifting yourself..."
+			else:
+				reason = "User ID '{{ userid }}' not found... Make sure you entered a valid Discord ID.<br>Or the user have not started playing OV Bot yet..."
+			return render(request, "Cannot-find-user.html", {'userid': userid, 'reason': reason})
 		else:
 			gifter = cll.find_one({"id": gifterid})
 			user = cll.find_one({"id": userid})
