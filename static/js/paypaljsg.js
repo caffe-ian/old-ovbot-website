5c9d100d223964e4ecc09d38d38c2dcecfe90059
Chock1 <78070496+Chock1@users.noreply.github.com>
2022-01-10 22:30:08 +0800

    Update paypaljsg.js

--- static/js/paypaljsg.js
+++ static/js/paypaljsg.js
@@ -1,21 +1,4 @@
-function getCookie(name) {
-    let cookieValue = null;
-    if (document.cookie && document.cookie !== '') {
-        const cookies = document.cookie.split(';');
-        for (let i = 0; i < cookies.length; i++) {
-            const cookie = cookies[i].trim();
-            // Does this cookie string begin with the name we want?
-            if (cookie.substring(0, name.length + 1) === (name + '=')) {
-                cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                break;
-            }
-        }
-    }
-    return cookieValue;
-}
-const csrftoken = getCookie('csrftoken');
-
-      function initPayPalButton() {
+function initPayPalButton() {
         var itemOptions = document.querySelector("#smart-button-container #item-options");
     var quantity = parseInt(20);
     var quantitySelect = document.querySelector("#smart-button-container #quantitySelect");
