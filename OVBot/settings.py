93495cc2fb8568a6b02d9c0e9e08b56d7b0dd1c9
Chock1 <78070496+Chock1@users.noreply.github.com>
2022-12-28 14:17:45 +0800

    Update settings.py

--- OVBot/settings.py
+++ OVBot/settings.py
@@ -134,7 +134,6 @@ STATIC_ROOT = 'static_root/'
 
 STATICFILES_DIRS = [
     'static/'
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
